cc5120d99542614a00873b8c3117d818c6c0984c
Edwin Peraza <edwinperaza87@gmail.com>
2020-03-15 22:43:55 -0700

    Update gradle and library versions

--- app/src/main/java/cl/blackmind/app/ui/TaskFragment.kt
+++ app/src/main/java/cl/blackmind/app/ui/TaskFragment.kt
@@ -1,88 +1,88 @@
 package cl.blackmind.app.ui
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import cl.blackmind.app.R
-
 import cl.blackmind.app.domain.TaskTestRepository
 
 class TaskFragment : Fragment(),
     TaskAdapter.TaskAdapterOnItemClickHandler {
 
-  private lateinit var taskAdapter: TaskAdapter
-  private lateinit var recyclerView: RecyclerView
-  private lateinit var loading: ProgressBar
-  private lateinit var viewModel: TaskViewModel
+    private lateinit var taskAdapter: TaskAdapter
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var loading: ProgressBar
+    private lateinit var viewModel: TaskViewModel
 
-  private var position = RecyclerView.NO_POSITION
+    private var position = RecyclerView.NO_POSITION
 
-  override fun onViewCreated(
-    view: View,
-    savedInstanceState: Bundle?
-  ) {
-    super.onViewCreated(view, savedInstanceState)
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?
+    ) {
+        super.onViewCreated(view, savedInstanceState)
 
-    loading = view.findViewById(R.id.loading)
+        loading = view.findViewById(R.id.loading)
 
-    taskAdapter = TaskAdapter(this)
+        taskAdapter = TaskAdapter(this)
 
-    val gridLayoutManager = StaggeredGridLayoutManager(2, RecyclerView.VERTICAL)
-        .apply {
-          gapStrategy = StaggeredGridLayoutManager.GAP_HANDLING_NONE
-        }
+        val gridLayoutManager = StaggeredGridLayoutManager(2, RecyclerView.VERTICAL)
+            .apply {
+                gapStrategy = StaggeredGridLayoutManager.GAP_HANDLING_NONE
+            }
 
-    recyclerView = view.findViewById<RecyclerView>(R.id.taskRecycler)
-        .apply {
-          layoutManager = gridLayoutManager
-          setHasFixedSize(true)
-          adapter = taskAdapter
-        }
+        recyclerView = view.findViewById<RecyclerView>(R.id.taskRecycler)
+            .apply {
+                layoutManager = gridLayoutManager
+                setHasFixedSize(true)
+                adapter = taskAdapter
+            }
 
-    viewModel = ViewModelProviders.of(this,
-        TaskViewModelFactory(TaskTestRepository())
-    )
-        .get(TaskViewModel::class.java)
+        viewModel = ViewModelProvider(
+            this,
+            TaskViewModelFactory(TaskTestRepository())
+        )
+            .get(TaskViewModel::class.java)
 
-    viewModel.getAllTask()
-        .observe(this, Observer<List<TaskEntity>> { entities: List<TaskEntity> ->
-          taskAdapter.updateTasks(entities)
-          if (position == RecyclerView.NO_POSITION) position = 0
-          recyclerView.smoothScrollToPosition(position)
+        viewModel.getAllTask()
+            .observe(viewLifecycleOwner, Observer<List<TaskEntity>> { entities: List<TaskEntity> ->
+                taskAdapter.updateTasks(entities)
+                if (position == RecyclerView.NO_POSITION) position = 0
+                recyclerView.smoothScrollToPosition(position)
 
-          if (entities.isNotEmpty())
-            showTasksDataView()
-          else
-            showLoading()
-        })
-  }
+                if (entities.isNotEmpty())
+                    showTasksDataView()
+                else
+                    showLoading()
+            })
+    }
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View? {
-    return inflater.inflate(R.layout.task_fragment, container, false)
-  }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.task_fragment, container, false)
+    }
 
-  private fun showTasksDataView() {
-    loading.visibility = View.INVISIBLE
-    recyclerView.visibility = View.VISIBLE
-  }
+    private fun showTasksDataView() {
+        loading.visibility = View.INVISIBLE
+        recyclerView.visibility = View.VISIBLE
+    }
 
-  private fun showLoading() {
-    recyclerView.visibility = View.INVISIBLE
-    loading.visibility = View.VISIBLE
-  }
+    private fun showLoading() {
+        recyclerView.visibility = View.INVISIBLE
+        loading.visibility = View.VISIBLE
+    }
 
-  override fun onItemClick(title: String) {
+    override fun onItemClick(title: String) {
 
-  }
+    }
 }
--- core/src/main/kotlin/com/up/core/extension/ConnectivityManager.kt
+++ core/src/main/kotlin/com/up/core/extension/ConnectivityManager.kt
@@ -1,5 +1,2 @@
 package com.up.core.extension
 
-import android.net.ConnectivityManager
-
-fun ConnectivityManager.isNetworkAvailable() = activeNetworkInfo?.isConnected == true
